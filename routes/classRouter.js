4fe0e325c8cf810cf89a299f58812c6c9d69c074
Suyash Dubey <suyash880.be22@chitkara.edu.in>
2025-08-17 10:41:29 +0530

    added leave feature for onging class + connected routes with server.js

--- routes/classRouter.js
+++ routes/classRouter.js
@@ -2,9 +2,10 @@ const express=require('express');
 const router=express.Router();
 const isMember = require('../middleware/isMember');
 const verifyTeacher = require('../middleware/TeacherAuth.js');
-const {enterClass,saveClassActivity}=require('../controllers/classActivityCont');
+const {enterClass,saveClassActivity,leaveClass}=require('../controllers/classActivityCont');
 
 router.post('/enter/:id', isMember, enterClass);
-router.post('/save/:classId', verifyTeacher, isMember, saveClassActivity);
+router.put('/save/:classId', verifyTeacher, isMember, saveClassActivity);
+router.put('/leave/:id', isMember, leaveClass);
 
 module.exports = router;

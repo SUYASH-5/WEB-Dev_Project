e693bfdfd5e47d5b50aada85d4c58e3d85d177a9
Suyash Dubey <suyash880.be22@chitkara.edu.in>
2025-08-17 10:04:57 +0530

    Forgot to add save feature for onging class

--- routes/classRouter.js
+++ routes/classRouter.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const router=express.Router();
 const isMember = require('../middleware/isMember');
-const {enterClass}=require('../controllers/classActivityCont');
+const verifyTeacher = require('../middleware/TeacherAuth.js');
+const {enterClass,saveClassActivity}=require('../controllers/classActivityCont');
 
 router.post('/enter/:id', isMember, enterClass);
+router.post('/save/:classId', verifyTeacher, isMember, saveClassActivity);
 
 module.exports = router;
